635a75bcf494be26924e1096b0fd8a90e42d744d
aarti <aartikanojiya9131@gmail.com>
2025-10-14 18:24:33 +0530

    adding first program of datatype

---
+++ JavaExercises/Datatype programs/program1.java
@@ -0,0 +1,29 @@
+public class Program1 {
+    public static void main(String[] args) {
+        // Integer types
+        byte b = 100;
+        short s = 10000;
+        int i = 123456;
+        long l = 123456789L;
+
+        // Floating-point types
+        float f = 5.75f;
+        double d = 19.99;
+
+        // Character type
+        char c = 'A';
+
+        // Boolean type
+        boolean isJavaFun = true;
+
+        // Print all values
+        System.out.println("Byte: " + b);
+        System.out.println("Short: " + s);
+        System.out.println("Int: " + i);
+        System.out.println("Long: " + l);
+        System.out.println("Float: " + f);
+        System.out.println("Double: " + d);
+        System.out.println("Char: " + c);
+        System.out.println("Boolean: " + isJavaFun);
+    }
+}
